11a43f69f7067a9bb4a49e187463f80a51fddf1f
Ishara Samuditha <developer.ishara@gmail.com>
2025-04-10 11:50:57 +0530

    feat: add multi part thumbnail upload

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
+	"io"
+	"net/http"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
@@ -28,10 +28,59 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+	}
+	defer file.Close()
+	mediaType := header.Header.Get("Content-Type")
+
+	if mediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "Missing Content-Type for thumbnail", nil)
+		return
+	}
+
+	imageData, err := io.ReadAll(file) //
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read file", err)
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to find video", err)
+	}
+
+	if userID != video.UserID {
+		respondWithError(w, http.StatusUnauthorized, "You are not authorized to upload this thumbnail", nil)
+	}
+
+	thumbnail := thumbnail{
+		mediaType: mediaType,
+		data:      imageData,
+	}
+
+	videoThumbnails[videoID] = thumbnail
+
+	ThumbnailURL := fmt.Sprintf("http://localhost:%v/api/thumbnails/%v\n\n", cfg.port, videoID)
+
+	fmt.Println(ThumbnailURL)
+
+	video.ThumbnailURL = &ThumbnailURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		delete(videoThumbnails, videoID)
+
+		respondWithError(w, http.StatusInternalServerError, "Unable to save video", err)
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
